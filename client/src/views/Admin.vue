8557ffbf397aad4ea561abe0ca9ded2db485ca72
StevenReale <smreale78@gmail.com>
2023-08-24 11:52:32 -0400

    Further UI tweak

--- client/src/views/Admin.vue
+++ client/src/views/Admin.vue
@@ -3,10 +3,11 @@
 
     <h2 class="hide-mobile">Deck Management</h2>
 
+    <div id="intro-div">
     <p id="login-prompt" v-if="!isLoggedIn">Feel free to explore the app's functionality with this dummy data. If you wish to create a persistent card deck, please <router-link :to="{name: 'login'}">login</router-link> or <router-link :to="{name: 'register'}">register</router-link>.</p>
     <p>Flashcards begin in Bin 0. Each time you answer correctly, it moves into the next bin, where there is a longer delay until the card becomes available again. Each time you answer incorrectly, it moves back to Bin 1.</p>
     <p>Once a card lands in Bin 11, or is answered incorrectly 10 times, it becomes inactive</p>
-    
+    </div>
     
     <button id="create" @click.prevent="create">Create New Card</button>
     <flashcard-collection />
@@ -50,6 +51,12 @@ export default {
 p {
   margin-bottom: 1em;
   font-style: italic;
+}
+
+#intro-div {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
 }
 
 @media (max-width: 576px) {
